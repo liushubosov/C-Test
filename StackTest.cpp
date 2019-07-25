14600e2148edba81f2b34e2c30180c0afa9d61ab
liushubosov <liushubosov@gmail.com>
2019-07-25 21:44:59 +0800

    find the maximum number of points

--- StackTest.cpp
+++ StackTest.cpp
@@ -1,7 +1,7 @@
-#include <string>
 #include <iostream>
-#include <stack>
+#include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
@@ -9,39 +9,103 @@ using namespace std;
 
 struct Point
 {
-    int x;
-    int y;
-    Point(){
-        x = 0;
-        y = 0;
-    }
+	int x;
+	int y;
+	Point() {
+		x = 0;
+		y = 0;
+	}
 
-    Point(int a, int b){
-        x = a;
-        y = b;
-    }
+	Point(int a, int b) {
+		x = a;
+		y = b;
+	}
 
 };
 
-class Solution
-{
+class Solution {
 public:
-    int doSomething(vector<Point> &points)
-    {
-        Point base = points[0];
-        vector<float> results;
-        for(int i=1;i < points.size();i++)
-        {
-            float k = (points[i].y - base.y) / (points[i].x - base.x);
-            results.push_back(abs(k));
-        }
-
-        return 0;
-    }
+	int maxPoints(vector<Point> &points) {
+		if (points.size() == 0)
+			return 0;
+		
+		int nMax = 0;
+		for (int i = 0; i < points.size(); i++)
+		{
+			map<float, int>  results;
+			Point base = points[i];
+			int nBase = 0;
+			int nLineMax = 0;
+			for (int j = i + 1; j < points.size(); j++)
+			{
+				if (points[j].y == base.y && points[j].x == base.x)
+				{
+					nBase++;
+					continue;
+				}
+				float a = points[j].y - base.y;
+				float b = points[j].x - base.x;
+				float k = 0;
+				if (b != 0)
+					k = a / b;
+				map<float, int>::iterator iter1;
+				iter1 = results.find(k);
+				// no key
+				if (iter1 == results.end())
+				{
+					pair<float, int> value(k, 1);
+					results.insert(value);
+					if (nLineMax < 1)
+						nLineMax = 1;
+				}
+				else
+				{
+					iter1->second++;
+					if (iter1->second > nLineMax)
+						nLineMax = iter1->second;
+				}
+			}
+			nLineMax = nLineMax + 1 + nBase;
+			if (nLineMax > nMax)
+				nMax = nLineMax;
+		}
+		//nMax = nMax + 1 + nBase;
+		return nMax;
+	}
 };
 
 int main()
 {
-    cout << "hello" << endl;
+	cout << "hello" << endl;
+	Solution solut;
+	vector<Point> points;
+	Point p1, p2, p3, p4, p5;
+	p1.x = -4;
+	p1.y = -4;
+	p2.x = -8;
+	p2.y = -582;
+	p3.x = -3;
+	p3.y = 3;
+	p4.x = -9;
+	p4.y = -651;
+	p5.x = 9;
+	p5.y = 591;
+	points.push_back(p1);
+	//points.push_back(p2);
+	//points.push_back(p3);
+	//points.push_back(p4);
+	//points.push_back(p5);
+	int nMax = solut.maxPoints(points);
+	cout << "max value = " << nMax << endl;
+
+	//double fV = 0.0;
+	//fV = 1.0 / 3.0;
+	//cout << fV << endl;
+
+	//遍历 map， 找出最大值
+
+
+	system("pause");
     return 0;
 }
+
