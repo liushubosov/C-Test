f17c26e9c4a7f851c879e83fdd711907a682db58
liushubosov <liushubosov@gmail.com>
2019-07-28 23:53:50 +0800

    leetcode 加一

--- StackTest.cpp
+++ StackTest.cpp
@@ -259,6 +259,34 @@ public:
         }
         return i;
     }
+
+/**
+ * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ */
+	vector<int> plusOne(vector<int>& digits) {
+        bool bNext = false;
+        for(int i=digits.size()-1;i >= 0;i--)
+        {
+            if(digits[i] == 9)
+            {
+                digits[i] = 0;
+                bNext = true;
+            }
+            else
+            {
+                digits[i]++;
+                bNext = false;
+                break;
+            }
+            
+        }
+        if(bNext)
+            digits.insert(digits.begin(),1);
+        return digits;
+    }
+
 };
 
 int main()
