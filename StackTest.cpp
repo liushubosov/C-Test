0734f3c6067b7284709a67285dc9f3454e7851a4
liushubosov <liushubosov@gmail.com>
2019-08-01 23:36:51 +0800

    对称二叉树

--- StackTest.cpp
+++ StackTest.cpp
@@ -360,6 +360,23 @@ public:
         return result;
     }
 
+	/**
+	 * 给定一个二叉树，检查它是否是镜像对称的。
+	 */
+	bool isSymmetric(TreeNode* root) {
+        
+        return traverse(root,root);
+    }
+    
+    bool traverse(TreeNode* p1, TreeNode* p2)
+    {
+        if(!p1 && !p2) return true;
+        if(!p1 || !p2) return false;
+        
+        return (p1->val == p2->val) && traverse(p1->left,p2->right) 
+            && traverse(p1->right,p2->left);
+    }
+
 };
 
 int main()
