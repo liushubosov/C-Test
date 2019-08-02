5fd2d6fb326296b3c831ced803401e0d4e61e40b
liushubosov <liushubosov@gmail.com>
2019-08-02 23:32:22 +0800

    119. 杨辉三角 II

--- StackTest.cpp
+++ StackTest.cpp
@@ -377,6 +377,23 @@ public:
             && traverse(p1->right,p2->left);
     }
 
+	/**
+	 * 119. 杨辉三角 II
+	 * 给定一个非负索引 k，其中 k ≤ 33，返回杨辉三角的第 k 行。
+	 */
+	vector<int> getRow(int rowIndex) {
+        vector<int> result(rowIndex+1,0);
+        result[0] = 1;
+        for(int i=1;i < rowIndex + 1;i++)
+        {
+            for(int j=i;j >= 1;j--)
+            {
+                result[j] = result[j-1] + result[j];
+            }                                    
+        }
+        return result;
+    }
+
 };
 
 int main()
