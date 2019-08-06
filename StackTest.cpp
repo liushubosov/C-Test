e05026d1cc63ed920377fc24e0d3394665b4dec3
liushubosov <liushubosov@gmail.com>
2019-08-06 22:59:34 +0800

    169. 求众数

--- StackTest.cpp
+++ StackTest.cpp
@@ -452,6 +452,27 @@ public:
         return a;
     }
 
+	/**
+	 * 169. 求众数
+	 * 给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+	 * 你可以假设数组是非空的，并且给定的数组总是存在众数。
+	 * Boyer-Moore 投票算法
+	 */
+	int majorityElement(vector<int>& nums) {
+        int res = 0;
+        int count = 0;
+        for(int i=0;i < nums.size();i++)
+        {
+            if(count == 0)
+                res = nums[i];
+            if(res == nums[i])
+                count++;
+            else
+                count--;
+        }
+        return res;
+    }
+
 };
 
 int main()
