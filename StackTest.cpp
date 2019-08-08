f284148750e7ba257e3c514ef3298cb4e6582869
liushubosov <liushubosov@gmail.com>
2019-08-08 23:47:47 +0800

    167. 两数之和 II - 输入有序数组

--- StackTest.cpp
+++ StackTest.cpp
@@ -488,6 +488,37 @@ public:
         reverse(nums.begin(), nums.end());
     }
 
+    /**
+     * 167. 两数之和 II - 输入有序数组
+     * 给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+     * 函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+     * 说明:
+     * 返回的下标值（index1 和 index2）不是从零开始的。
+     * 你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+     * Java
+     * public int[] twoSum(int[] numbers, int target) {
+        int low = 0;
+        int high = numbers.length - 1;
+        while(low < high)
+        {     
+            int sum = numbers[low] + numbers[high];
+            if(sum < target)
+            {
+                low++;
+            }else if(sum == target)               
+            {
+                int[] res = new int[2];
+                res[0] = ++low;
+                res[1] = ++high;
+                return res;
+            }else
+                high--;
+        }
+        return new int[0];
+    }
+     */
+
+
 };
 
 int main()
