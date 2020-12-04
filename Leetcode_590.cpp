017e1ae3079ba5636300362eae730924df8dd9ce
liushubosov <liushubosov@gmail.com>
2020-12-05 00:20:40 +0800

    590. N叉树的后序遍历(增加迭代方法)

--- Leetcode_590.cpp
+++ Leetcode_590.cpp
@@ -52,5 +52,52 @@ public:
 };
 
 
-//迭代
-// 暂时没写
+//迭代1
+// 与先根迭代代码类似，只是往栈中存孩子节点的时候，不再从后往前存，改为从前往后存。
+// 这是因为数组最后需要反转一下
+class Solution2 {
+public:    
+    vector<int> postorder(Node* root) {            
+        if(!root)
+            return {};
+        vector<int> res;
+        stack<Node*> myStack;
+        myStack.push(root);
+        while(!myStack.empty()){
+            Node* p = myStack.top();
+            res.push_back(p->val);
+            myStack.pop();
+            for(int i=0;i < p->children.size();i++){
+                myStack.push(p->children[i]);
+            }
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
+
+//迭代2
+// 破坏了原结构（把节点的孩子节点全清空了），不推荐
+class Solution {
+public:    
+    vector<int> postorder(Node* root) {            
+        if(!root)
+            return {};
+        vector<int> res;
+        stack<Node*> myStack;
+        myStack.push(root);
+        while(!myStack.empty()){
+            Node* p = myStack.top();
+            if(p->children.size() == 0){
+                res.push_back(p->val);
+                myStack.pop();
+            }else{
+                for(int i=p->children.size()-1;i>=0 ;i--){
+                    myStack.push(p->children[i]);
+                }
+                p->children.clear();
+            }
+        }
+        return res;
+    }
+};
