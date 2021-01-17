62581de11f6754b22366f2c7f468624fe36325b2
liushubosov <liushubosov@gmail.com>
2021-01-17 17:02:58 +0800

    575. 分糖果 代码提交错误修正

--- Leetcode_575.cpp
+++ Leetcode_575.cpp
@@ -11,27 +11,18 @@
 * 
 * 解题思路： 
 *   用 set 计算糖果种类
-*   妹妹可以分到 n/2 个糖， 所以如果糖种类超过 n/2, 妹妹也分不到，应该立即 return n/2;
+*   妹妹最多可以分到 n/2 个糖， 因为是两个人平均分配，如果糖种类超过 n/2, 妹妹也分不到，应该立即 return n/2;
 */
 
-string addStrings(string num1, string num2)
-{
-    string res;
-    int i = num1.size() - 1;
-    int j = num2.size() - 1;
-    int add = 0;
-    while (i >= 0 || j >= 0 || add != 0)
-    {
-        int a, b;
-        a = i < 0 ? 0 : num1[i] - '0';
-        b = j < 0 ? 0 : num2[j] - '0';
-        int sum = a + b + add;
-        res.push_back(sum % 10 + '0');
-        add = sum / 10;
-        i--;
-        j--;
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        //unordered_set<int> mySet(candyType.begin(),candyType.end());
+        unordered_set<int>  mySet;
+        size_t candyLen = candyType.size();
+        for(int i=0;i < candyLen && mySet.size() < candyLen/2;++i){
+            mySet.insert(candyType[i]);
+        }
+        return mySet.size();
     }
-
-    reverse(res.begin(), res.end());
-    return res;
-}
+};
