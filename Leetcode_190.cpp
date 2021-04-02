9f5a0c0ca797f8ef066fe1c12cd7efea361fa524
liushubosov <liushubosov@gmail.com>
2021-04-02 23:29:35 +0800

    190. 颠倒二进制位

--- Leetcode_190.cpp
+++ Leetcode_190.cpp
@@ -15,7 +15,7 @@ public:
     uint32_t reverseBits(uint32_t n) {
 		uint32_t res = 0;
         int index = 32; //操作32次移位操作
-        while(index--)
+        while(--index)
         {
             res<<=1;//结果左移一位，空出位置与n最后一位相加
             res+=n&1; //加上n的最后一位
