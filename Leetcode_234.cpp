6fb7bc245481528430a0b6c10a5068c4af8ad00a
liushubosov <liushubosov@gmail.com>
2021-04-18 23:48:34 +0800

    234. 回文链表

---
+++ Leetcode_234.cpp
@@ -0,0 +1,32 @@
+/*
+* 234. 回文链表
+*
+请判断一个链表是否为回文链表。
+
+* 
+* 解题思路： 
+*   迭代，函数外保存前指针。
+*/
+
+
+class Solution {
+    ListNode* frontPointer;
+public:
+    bool recursivelyCheck(ListNode* currentNode) {
+        if (currentNode != nullptr) {
+            if (!recursivelyCheck(currentNode->next)) {
+                return false;
+            }
+            if (currentNode->val != frontPointer->val) {
+                return false;
+            }
+            frontPointer = frontPointer->next;
+        }
+        return true;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        frontPointer = head;
+        return recursivelyCheck(head);
+    }
+};
