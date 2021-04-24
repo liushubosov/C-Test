11a467ee38556d5a96a18095deb7fc9d92c3e44c
liushubosov <liushubosov@gmail.com>
2021-04-24 23:59:09 +0800

    237. 删除链表中的节点

---
+++ Leetcode_237.cpp
@@ -0,0 +1,26 @@
+/*
+* 237. 删除链表中的节点
+*
+请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点。传入函数的唯一参数为 要被删除的节点 。
+
+*  
+* 解题思路：
+*   把后继指针的值移到当前指针，删除后继指针
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
